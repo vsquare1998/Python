43e98a172dde128524c1fa6166b6997df06f717b
Vaibhav Vishal <vaibhavvishal1244@gmail.com>
2020-06-22 14:01:12 +0530

    added project covid-19 notifier

---
+++ Projects/Covid-19_notifier.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+from win10toast import ToastNotifier
+
+source = requests.get("https://www.worldometers.info/coronavirus/country/india/").text
+
+soup = BeautifulSoup(source, "lxml")
+
+# for article in soup.find_all("div", id="maincounter-wrap"):
+#     try:
+#         heading = article.h1.text
+#         print(heading)
+
+#         cases = article.div.span.text
+#         print(cases)
+#     except Exception as e:
+#         continue
+
+date = soup.find("div", class_="news_date")
+
+cases = soup.find("li", class_="news_li")
+new_case = cases.strong.text.split()[0]
+# print(f"new_case : {new_case}")
+
+deaths = list(cases.strong.next_siblings)[1].text.split()[0]
+# print(f"deaths: {deaths}")
+
+notifier = ToastNotifier()
+message = f"New Cases: {new_case} \n New Deaths: {deaths}"
+notifier.show_toast(title="Covid-19 Update", msg=message, duration=5)
+
